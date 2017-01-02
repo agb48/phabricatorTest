7fa8ec96b206c7b4672333d5b378e965a6f3e465
German Barros <german.barros@moove-it.com>
2017-01-02 16:18:03 -0300

    first commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by gbarros on 1/2/17.
+ */
+public class PersonTest {
+
+    @Test
+    @Ignore
+    public void shouldFail() {
+        fail("Test to fail");
+    }
+
+    @Test
+    @Ignore
+    public void shouldBeSuccess() {
+        fail("Test to fail");
+    }
+
+}
